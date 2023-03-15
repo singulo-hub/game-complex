2f21221f70b3d2615584611ce4b320f53192cdeb
James <jblack144@gmail.com>
2023-03-14 22:39:30 -0400

    Add icons

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.less']
 })
 export class CardComponent {
+  title: string = 'Placeholder';
+  icons: any[] = [];
+  description: string = 'You should probably change this.';
 
+  constructor() {
+
+  }
 }
